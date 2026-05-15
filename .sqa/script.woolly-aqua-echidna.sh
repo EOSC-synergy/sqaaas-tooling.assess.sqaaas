766a5c694f1aee2da26ecdec165c485c0dd165c3
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2026-05-15 06:53:09 +0100

    Add JePL file structure

--- .sqa/script.woolly-aqua-echidna.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd github.com/EOSC-synergy/sqaaas-tooling &&
-    hadolint QC.Doc/Dockerfile QC.Doc/restructuredtext-lint/Dockerfile QC.Doc/markdownlint/Dockerfile QC.Sec/brakeman/Dockerfile QC.Sec/bandit/Dockerfile QC.Sec/gosec/Dockerfile QC.Sty/eslint/Dockerfile QC.Sty/rubocop/Dockerfile QC.Sty/stylelint/Dockerfile QC.Sty/pycodestyle/Dockerfile QC.Sty/jsonlint/Dockerfile QC.Sty/staticcheck/Dockerfile QC.FAIR/Dockerfile QC.Uni/pytest/Dockerfile QC.Ver/Dockerfile QC.Met/checkCitable/Dockerfile QC.Met/cff-converter-python/Dockerfile QC.Lic/Dockerfile QC.Lic/licensee/Dockerfile --failure-threshold error
-)
